76c94d996c9d7530cbe5fca9fa82f3c63da7d9fc
newton-c <cnewton2@buffalo.edu>
2021-06-22 16:33:17 -0400

    fixing bad syntax to make code readable

--- scripts/R/matching.R
+++ scripts/R/matching.R
@@ -6,11 +6,11 @@ library(modelsummary)
 d <- read_dta("data/hale_data.dta")
 d$pko_binary <- ifelse(d$pko_years > 0, 1, 0)
 
-match_data <- d %>% select(year, ccode, hale, pko_years, pko_binary,
-                     total_violence_1000, osv_1000, brd_1000, pkoxdeath,
-                     pkoxosv, pkoxbrd, hdi_knn, helog_knn, civilwarborder,
-                     urbangrowth_knn, gini_knn, tropical, xpolity_knn,
-                     ef_knn, y05, y10, y15)
+match_data <- d %>%
+    select(year, ccode, hale, pko_years, pko_binary, total_violence_1000,
+           osv_1000, brd_1000, pkoxdeath, pkoxosv, pkoxbrd, hdi_knn, helog_knn,
+           civilwarborder, urbangrowth_knn, gini_knn, tropical, xpolity_knn,
+           ef_knn, y05, y10, y15)
 
 cem_data <- matchit(pko_binary ~ total_violence_1000 + tropical +
                     xpolity_knn + year, data = match_data, method = "cem")
